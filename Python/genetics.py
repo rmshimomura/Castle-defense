f242fe4392a7207ea05db0d082a60eaeb339a2cf
rmshimomura <shimomurarodrigo1@gmail.com>
2022-11-22 15:42:52 -0300

    Representação da população

--- Python/genetics.py
+++ Python/genetics.py
@@ -4,6 +4,18 @@ class Chromosomes():
         self.raw_data = []
         self.genes = []
         self.fitness = 0
+        self.health = 0
 
     def extract_genes(self):
-        self.genes = []
+        for wall in self.raw_data.walls:
+            self.genes.append([wall.attack_points, wall.defense_points])
+
+def represent_population(population):
+    chromosomes = []
+    for castle in population:
+        chromosome = Chromosomes()
+        chromosome.raw_data = castle
+        chromosome.health = castle.health_points
+        chromosome.extract_genes()
+        chromosomes.append(chromosome)
+    return chromosomes
