b007795ad3c4df4842df4718587411bfcc32a870
rmshimomura <shimomurarodrigo1@gmail.com>
2022-11-23 00:19:44 -0300

    Adicionado mutação e redistribuição de pontos

--- Python/genetics.py
+++ Python/genetics.py
@@ -53,45 +53,59 @@ def tournament_selection(population, tournament_size):
     selected_chromosomes.sort(key=lambda x: x.fitness, reverse=True)
     return selected_chromosomes
     
-def redistribute_points(chromosome, sum_points, MAX_VALUE, change_point, mode):
+def redistribute_points(chromosome, sum_points, MAX_VALUE, mode, MINIMUM_VALUE):
 
     difference = sum_points - MAX_VALUE
 
-    indexes = [i for i in range(0, len(chromosome.genes)) if i != change_point]
-
-    difference_to_each_side = abs(difference) // 3
-
     if difference > 0:
 
+        i = 0
 
-        for index in indexes:
-            chromosome.genes[index][mode] -= abs(difference_to_each_side)
-        if difference % 3 != 0:
-            compensate_index = random.choice(indexes)
-            chromosome.genes[compensate_index][mode] -= abs(difference) % 3
+        while difference > 0:
 
+            if chromosome.genes[i][mode] - 1 >= MINIMUM_VALUE:
+                chromosome.genes[i][mode] -= 1
+            difference -= 1
+
+            i += 1
+
+            if i == len(chromosome.genes):
+                i = 0
+    
     elif difference < 0:
 
-        for index in indexes:
-            chromosome.genes[index][mode] += abs(difference_to_each_side)
-        if difference % 3 != 0:
-            compensate_index = random.choice(indexes)
-            chromosome.genes[compensate_index][mode] += abs(difference) % 3
+        for i in range(0, len(chromosome.genes)):
 
-def check_sum_chromosome(chromosome, MAXIMUM_ATK_VALUE, MAXIMUM_DEF_VALUE, change_point):
+            while chromosome.genes[i][mode] < MINIMUM_VALUE:
+                chromosome.gen[i][mode] += 1
+                difference += 1
+
+        i = 0
+
+        while difference < 0:
+
+            chromosome.genes[i][mode] += 1
+            difference += 1
+
+            i += 1
+
+            if i == len(chromosome.genes):
+                i = 0
+
+def check_sum_chromosome(chromosome, MAXIMUM_ATK_VALUE, MAXIMUM_DEF_VALUE, MINIMUM_VALUE):
 
     sum_attack = sum([gene[0] for gene in chromosome.genes])
     sum_defense = sum([gene[1] for gene in chromosome.genes])
 
     if sum_attack != MAXIMUM_ATK_VALUE:
 
-        redistribute_points(chromosome, sum_attack, MAXIMUM_ATK_VALUE, change_point, mode = 0)
+        redistribute_points(chromosome, sum_attack, MAXIMUM_ATK_VALUE, 0, MINIMUM_VALUE)
 
     if sum_defense != MAXIMUM_DEF_VALUE:
 
-        redistribute_points(chromosome, sum_defense, MAXIMUM_DEF_VALUE, change_point, mode = 1)
+        redistribute_points(chromosome, sum_defense, MAXIMUM_DEF_VALUE, 1, MINIMUM_VALUE)
 
-def crossover(population, MAXIMUM_ATK_VALUE, MAXIMUM_DEF_VALUE):
+def crossover(population, MAXIMUM_ATK_VALUE, MAXIMUM_DEF_VALUE, MINIMUM_VALUE):
 
     for i in range(0, len(population), 2):
         if i + 1 < len(population):
@@ -100,8 +114,17 @@ def crossover(population, MAXIMUM_ATK_VALUE, MAXIMUM_DEF_VALUE):
             crossover_point = random.randint(1, len(first_chromosome.genes) - 1)
             first_chromosome.genes[crossover_point:], second_chromosome.genes[crossover_point:] = second_chromosome.genes[crossover_point:], first_chromosome.genes[crossover_point:]
 
-            check_sum_chromosome(first_chromosome, MAXIMUM_ATK_VALUE, MAXIMUM_DEF_VALUE, crossover_point)
-            check_sum_chromosome(second_chromosome, MAXIMUM_ATK_VALUE, MAXIMUM_DEF_VALUE, crossover_point)
+            check_sum_chromosome(first_chromosome, MAXIMUM_ATK_VALUE, MAXIMUM_DEF_VALUE, MINIMUM_VALUE)
+            check_sum_chromosome(second_chromosome, MAXIMUM_ATK_VALUE, MAXIMUM_DEF_VALUE, MINIMUM_VALUE)
 
         else:
             break
+
+def mutation(population, MAXIMUM_ATK_VALUE, MAXIMUM_DEF_VALUE, MUTATION_RATE, MINIMUM_VALUE):
+    for i in range(0, len(population), 1):
+        for j in range(0, 4, 1):
+            random_number = random.uniform(0, 1)
+            if(random_number <= MUTATION_RATE):
+                population[i].genes[j][0] = random.randint(MINIMUM_VALUE, MAXIMUM_ATK_VALUE)
+                population[i].genes[j][1] = random.randint(MINIMUM_VALUE, MAXIMUM_DEF_VALUE)
+            check_sum_chromosome(population[i], MAXIMUM_ATK_VALUE, MAXIMUM_DEF_VALUE, MINIMUM_VALUE)
